4640158a376625ef6ebee44d7d72565677525965
Pavel Dinh <pavel.dinh1@gmail.com>
2019-01-10 16:48:26 +0100

    update VS project

---
+++ InventorySystem/InventorySystem/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Button(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void Login_Button(object sender, EventArgs e)
+        {
+            //TODO Check if valid login
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=InventorySystem;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter(@"SELECT *
+                FROM[dbo].[Login] Where UserName = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password... or both !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Clear_Button(sender, e);
+            }
+        }
+    }
+}
---
+++ InventorySystem/InventorySystem/Products.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TODO Check if valid login
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=InventorySystem;Integrated Security=True");
+            // Insert Logic
+            con.Open();
+            bool status = false;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+                SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Products]
+           ([ProductCode]
+           ,[ProductName]
+           ,[ProductStatus])
+     VALUES
+           ('" + textBox1.Text + "','" + textBox2.Text + "','" + status + "');", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
